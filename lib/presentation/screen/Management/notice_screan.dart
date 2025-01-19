b96d869c2708a14f287ff2c82a3778f35edcec9a
MD Khalid Iqbal <khalidiqbal5257@gmail.com>
2025-01-20 00:08:28 +0600

    running project

---
+++ lib/presentation/screen/Management/notice_screan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoticeScrean extends StatefulWidget {
+  const NoticeScrean({super.key});
+
+  @override
+  State<NoticeScrean> createState() => _NoticeScreanState();
+}
+
+class _NoticeScreanState extends State<NoticeScrean> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
